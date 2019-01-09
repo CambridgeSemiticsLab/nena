3124bf8b99e3202c4070747fb5ac00fac9b365e4
Peter Heiner <p.heiner@gmail.com>
2019-01-09 05:37:33 +0000

    add dialect filtering to backend

--- api/views.py
+++ api/views.py
@@ -1,15 +1,20 @@
-from rest_framework import viewsets
-from .serializers import DialectListSerializer, DialectDetailSerializer, GrammarFeatureListSerializer, GrammarFeatureDetailSerializer
+from django.shortcuts import get_object_or_404
 
+from rest_framework import viewsets, filters, Response
+from django_filters import rest_framework as dfilters
+
+from .serializers import DialectListSerializer, DialectDetailSerializer, GrammarFeatureListSerializer, GrammarFeatureDetailSerializer
 from dialects.models import Dialect
 from grammar.models import Feature
-
 
 class DialectViewSet(viewsets.ModelViewSet):
     """
     API endpoint that allows dialects to be viewed or edited.
     """
     queryset = Dialect.objects.all()
+    filter_backends = (dfilters.DjangoFilterBackend, filters.OrderingFilter)
+    filter_fields = ('community', 'location')
+    ordering_fields = ('name', 'country', 'location')
 
     def get_serializer_class(self):
         """
@@ -22,7 +27,7 @@ class GrammarFeatureViewSet(viewsets.ModelViewSet):
     """
     API endpoint that allows grammar features to be viewed or edited.
     """
-    queryset = Feature.objects.filter(depth__lte=2)
+    queryset = Feature.objects.all()
 
     def get_queryset(self):
         return Feature.objects.filter(depth__lte=2) if self.action == 'list' else Feature.objects.all()
