193e7b24ea7c72bff8972531ff89a3726c3a2e04
James Strachan <jamespstrachan@gmail.com>
2019-02-20 14:38:38 +0000

    simple replacement of feature-detail view and template

--- grammar/views.py
+++ grammar/views.py
@@ -2,9 +2,11 @@ from django.views.generic.list import ListView
 from django.views.generic.detail import DetailView
 from django.utils.decorators import method_decorator
 from django.contrib.auth.decorators import login_required, user_passes_test
-from django.http import JsonResponse
-from django.db.models import Q
+from django.contrib.admin.views.decorators import staff_member_required
+from django.http import JsonResponse, HttpResponse
+from django.db.models import Prefetch
 from django.urls import reverse_lazy
+from django.shortcuts import render
 
 from grammar.models import Feature
 from dialects.models import Dialect, DialectFeature, DialectFeatureEntry, DialectFeatureExample
@@ -22,41 +24,33 @@ class FeatureListView(ListView):
         return context
 
 
-class FeatureDetailView(DetailView):
-    '''Details for a specific grammar feature across all dialects'''
+@staff_member_required
+def feature(request, pk):
+    ''' Faster way to list all dialects with corresponding entries for a given feature '''
+    ''' todo - replace FeatureDetailView and FeatureParadigmView with this or similar '''
+    feature = Feature.objects.get(pk=pk)
+    prefetch_entries = Prefetch('entries', DialectFeatureEntry.objects.order_by('-frequency'))
+    context = {
+        'feature': feature,
+        'dialect_features': DialectFeature.objects.filter(feature_id=feature.id) \
+                                          .select_related('dialect') \
+                                          .prefetch_related(prefetch_entries)
+                                          .order_by('dialect__name'),
+    }
+    return render(request, 'grammar/feature_detail.html', context)
 
-    model = Feature
-
-    def get_context_data(self, **kwargs):
-        context = super(FeatureDetailView, self).get_context_data(**kwargs)
-        dialects = {}
-        features = DialectFeature.objects.filter(feature=self.kwargs['pk']).order_by('dialect')
-        for f in features:
-            mappable = True if (f.dialect.latitude and f.dialect.longitude) else False
-            entries = {}
-            for e in f.entries.all():
-                if e.feature.dialect.pk not in dialects:
-                    dialects[e.feature.dialect.pk] = {}
-                if e.frequency == 'P':
-                    entries['primary'] = e.entry
-                elif e.frequency == 'M':
-                    if 'marginal' in entries:
-                        entries['marginal'].append(e.entry)
-                    else:
-                        entries['marginal'] = [e.entry]
-                dialects[e.feature.dialect.pk] = {'name': e.feature.dialect.name, 'mappable': mappable, 'entries': entries }
-        context['dialects'] = dialects
-        return context
 
 class FeatureParadigmView(DetailView):
     '''Details for group of features across all dialects that form a language paradigm'''
 
     model = Feature
+    template_name = 'grammar/paradigm_detail.html'
 
     def get_context_data(self, **kwargs):
+
         context = super(FeatureParadigmView, self).get_context_data(**kwargs)
         dialects = {}
-        gf = DialectFeature.objects.filter(feature=self.kwargs['pk']).first().feature # the grammar feature
+        gf = Feature.objects.filter(pk=self.kwargs['pk']).first() # the grammar feature
         paradigm = [gf] + list(gf.get_children()) # list of all the paradigm features for the grammar feature
         for df in DialectFeature.objects.filter(feature=self.kwargs['pk']).order_by('dialect'):
             d = df.dialect
