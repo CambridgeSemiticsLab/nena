9cb82f8c58ae3a26f2aab627845104944c7ca7ab
James Strachan <jamespstrachan@gmail.com>
2021-03-10 11:12:59 +0000

    make wsgi file use production settings

--- common/wsgi.py
+++ common/wsgi.py
@@ -10,5 +10,5 @@ https://docs.djangoproject.com/en/1.11/howto/deployment/wsgi/
 import os
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "common.settings.local")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "common.settings.production")
 application = get_wsgi_application()
